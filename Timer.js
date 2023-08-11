dfe81bc52a48e3bec5a7341b3829e47b4d7ae217
khoibut <khoibut09@gmail.com>
2023-08-11 23:00:22 +0700

    Added the timer to the oven

---
+++ Timer.js
@@ -0,0 +1,23 @@
+time=610
+Oven=document.getElementById("OvenTimer");
+setInterval(() => {
+    minute=(time-time%60)/60;
+    minute=minute.toString();
+    if(minute.length<2){
+        minute="0"+minute;
+    }
+    second=time%60;
+    second=second.toString();
+    if(second.length<2){
+        second="0"+second;
+    }
+    currenttime=document.createTextNode(minute+":"+second)
+    if(document.getElementById("OvenTime")!=null){
+        Oven.removeChild(document.getElementById("OvenTime"))
+    }
+    Timer=document.createElement("div")
+    Timer.appendChild(currenttime)
+    Timer.setAttribute("id","OvenTime")
+    Oven.appendChild(Timer);
+    time--;
+}, 1000);
