fbf5716d4d3b4cc78296d7d300c010ceddf68ca0
khoibut <khoibut09@gmail.com>
2023-08-13 11:20:45 +0700

    Fixed basket spam, fixed merging problem

--- movingfunction.js
+++ movingfunction.js
@@ -3,37 +3,42 @@ const gear = document.getElementById("gear");
 const save = document.getElementById("save");
 const load = document.getElementById("load");
 let opacity=0;
-
+let clicked=false;
 document.getElementById("Basket").addEventListener("click", function movingfunction(){
     {
-        if(!moved){
-            moved=true;
-            document.getElementById("Basket").style.marginRight = "900px"
-            appear=setInterval(() => {
-                gear.style.opacity = opacity;
-                save.style.opacity = opacity;
-                load.style.opacity = opacity;
-                opacity+=0.1;
-                if(opacity >= 1){
-                    opacity=1;
-                    clearInterval(appear);
-                }
-                console.log(opacity);
-            }, 50);
-        }else{
-            moved=false;
-            disappear=setInterval(() => {
-                opacity-=0.1;
-                gear.style.opacity = opacity;
-                save.style.opacity = opacity;
-                load.style.opacity = opacity;
-                if(opacity <= 0){
-                    opacity=0;
-                    document.getElementById("Basket").style.marginRight = "0px";
-                    clearInterval(disappear);
-                }
-                console.log(opacity);
-            }, 5);
+        if(clicked==false){
+            clicked=true;
+            if(!moved){
+                document.getElementById("Basket").style.marginRight = "900px"
+                appear=setInterval(() => {
+                    opacity+=0.1;
+                    gear.style.opacity = opacity;
+                    save.style.opacity = opacity;
+                    load.style.opacity = opacity;
+                    console.log(opacity);
+                    if(opacity >= 1){
+                        clicked=false;
+                        moved=true;
+                        opacity=1;
+                        clearInterval(appear);
+                    }
+                }, 50);
+            }else{
+                disappear=setInterval(() => {
+                    opacity-=0.1;
+                    gear.style.opacity = opacity;
+                    save.style.opacity = opacity;
+                    load.style.opacity = opacity;
+                    if(opacity <= 0){
+                        clicked=false;
+                        moved=false;
+                        opacity=0;
+                        document.getElementById("Basket").style.marginRight = "0px";
+                        clearInterval(disappear);
+                    }
+                    console.log(opacity);
+                }, 5);
+            }
         }
     }
 });
